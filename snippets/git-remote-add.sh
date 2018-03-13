65a249bab01aaa90234a74680bb687d69b6dc261
TANG ZhiXiong <tang.zhi.xiong@qq.com>
2018-03-14 02:27:22 +0800

    update

---
+++ snippets/git-remote-add.sh
@@ -0,0 +1,4 @@
+git remote add origin git@gitlab.com:user/repoo.git
+git clone https://github.com/RainerKuemmerle/g2o.git
+git clone git@gitlab.com:shuhari/silly-task-2018.git
+git push -u origin master
