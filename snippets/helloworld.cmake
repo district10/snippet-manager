7e839d7ac5253bec76e97362fb5f49b646c52f12
TANG ZhiXiong <tang.zhi.xiong@qq.com>
2018-03-16 01:37:32 +0800

    update doc

--- snippets/helloworld.cmake
+++ snippets/helloworld.cmake
@@ -5,6 +5,11 @@ set( CMAKE_BUILD_TYPE "Debug" )
 set_source_files_properties(hello.c useHello.c PROPERTIES LANGUAGE CXX )
 
 include_directories( ${CMAKE_SOURCE_DIR}/includes )
+include_directories( ${CMAKE_BINARY_DIR} )
+configure_file(
+        "${PROJECT_SOURCE_DIR}/Configs.h.in"
+        "${PROJECT_BINARY_DIR}/Configs.h" )
+
 add_subdirectory( src )
 add_library( a STATIC a.cpp )
 add_library( b SHARED b.cpp )
@@ -15,6 +20,3 @@ target_link_libraries( ${PROJECT_NAME}  a b )
 file( GLOB SRCS *.c *.cpp *.cc *.h *.hpp )
 add_executable( ${PROJECT_NAME} ${SRCS} )
 
-configure_file(
-        "${PROJECT_SOURCE_DIR}/Configs.h.in"
-        "${PROJECT_BINARY_DIR}/Configs.h" )
