fa0e601dc35670c3111198d9ea5fa5cc3ca4469d
TANG ZhiXiong <tang.zhi.xiong@qq.com>
2018-03-18 22:05:50 +0800

    fix sophus (template version may cause some problem with non-template version

--- snippets/sophus.cmake
+++ snippets/sophus.cmake
@@ -1,4 +1,11 @@
-ckage( Sophus REQUIRED )
+find_package( Sophus REQUIRED )
 include_directories( ${Sophus_INCLUDE_DIRS} )
-
-target_link_libraries( exe ${Sophus_LIBRARIES} )
+IF( NOT Sophus_LIBRARIES )
+    SET( Sophus_LIBRARIES "libSophus.so" )
+ENDIF()
+message( STATUS "Sophus library status:" )
+message( STATUS "    includes: ${Sophus_INCLUDE_DIRS}" )
+message( STATUS "    libraries: ${Sophus_LIBRARIES}" )
+# -- Sophus library status:
+# --     includes: /usr/local/lib/cmake/Sophus/../../../include
+# --     libraries: libSophus.so
