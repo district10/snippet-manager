e060193b513fa69666723a13db1c15cd439346ce
TANG ZhiXiong <tang.zhi.xiong@qq.com>
2018-03-28 09:49:15 +0800

    updote snippets

--- snippets/identify-image-size.sh
+++ snippets/identify-image-size.sh
@@ -1,1 +1,1 @@
-identify *.png | awk '{printf "%-10s%s\n", $3, $1}' -
+identify *.png | awk '{printf "%-10s%s\n", $3, $1}' - # lspng, print filesize: w*h
---
+++ snippets/image-magick-transparent-margin.sh
@@ -0,0 +1,1 @@
+convert input.png  -background none -gravity north -splice 0x5 output.png # transporant margin
