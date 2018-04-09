2c51cbba90b8075091051caffd62b702b9460e54
TANG ZhiXiong <tang.zhi.xiong@qq.com>
2018-04-09 23:46:40 +0800

    more snippets

---
+++ snippets/initializer-list.cpp
@@ -0,0 +1,9 @@
+class MagicFoo {
+public:
+    std::vector<int> vec;
+    MagicFoo(std::initializer_list<int> list) {
+        for (std::initializer_list<int>::iterator it = list.begin();
+             it != list.end(); ++it)
+            vec.push_back(*it);
+    }
+};
