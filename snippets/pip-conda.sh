3fbd6323862e1d3a38f58db68187afc1af66c362
TANG ZhiXiong <zhixiong@momenta.ai>
2018-10-15 12:08:38 +0800

    more snippets

--- snippets/pip-conda.sh
+++ snippets/pip-conda.sh
@@ -3,6 +3,6 @@ python -m pip install --user numpy matplotlib opencv-python;
 
 # for anaconda users
 conda create --name py36_open3d python=3.6
-source activate py36_open3d
+source activate py36_open3d # windows: activate py36_open3d
 conda install -y numpy matplotlib
 conda install -y -c conda-forge opencv
