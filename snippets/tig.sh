a53c469bbf02a245b30523d828845ad7f0a2becc
TANG ZhiXiong <zhixiong@momenta.ai>
2018-12-13 22:33:21 +0800

    more snippets

---
+++ snippets/tig.sh
@@ -0,0 +1,5 @@
+sudo apt-get install libncurses5-dev
+git clone https://github.com/jonas/tig.git
+cd tig && make && make install
+
+
--- snippets/virtualbox.sh
+++ snippets/virtualbox.sh
@@ -5,3 +5,4 @@ virtualbox tips:
 - toggle keyboard: shift + hostkey
 - add shared folder
     in guest: sudo adduser $USER vboxsf
+- autoscale mode is best
