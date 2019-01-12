4529380c5467914865392a59997e2c8f16a6ede7
TANG ZHIXIONG <zhixiong@momenta.ai>
2019-01-12 21:13:26 +0800

    fix snippets

--- snippets/docker-image-ls.sh
+++ snippets/docker-image-ls.sh
@@ -1,6 +1,8 @@
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo chown $USER:docker /var/run/docker.sock
+# chmod 666 /var/run/docker.sock 
+# Is the docker daemon running on this host?
 sudo service docker restart
 newgrp - docker
 
--- snippets/openssh.sh
+++ snippets/openssh.sh
@@ -3,7 +3,7 @@ sudo apt-get install openssh-server
 # 修改配置，允许自己登陆
 sudo vi /etc/ssh/sshd_config # AllowUsers tzx
 
-# 添加自己的公钥 cp ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys，如果没有的话先用 ssh_keygen 生成密钥
+# 添加自己的公钥 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys，如果没有的话先用 ssh_keygen 生成密钥
 # 确保自己的 pub 可以已经存入：
 #       cat ~/.ssh/authorized_keys
 #           ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMLl/0cXodcoZFe+JLMN0Tb46DA5AKMAdGCPmOdDZfaJdTB/2OLNFHDdlzrqcnvP7L+Wg5+AGVabvzN3/bjcqCsACMPN6caT/HGeiS7c0Tx44AV3u15OZgy7tXCB8gmmr5PbANpcYrL4WBAmtBmBaer22It6lSGIiWy53cye8KX9sTLw8449KtCS95kiez6kfblBPA6bF9yPYykdInLw9pJAuPwk/AZDVOPAy68exVyV5D4+yb01OZqjGPrabp+Aq3NMbxgvaMZK1jyJE96WGjdr3ZY/Rr7NHp0sSCIqfMsy9wLQFvoQRhd8WpGwOQeByfmXmbYtd0mvAaIX1xiWkf dvorak4tzx@qq.com
